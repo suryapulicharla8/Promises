a13aa5b6d4887db9fbf7d2786cb8f68aa77bb944
suryapulicharla8 <suryapulicharla8@gmail.com>
2022-12-17 17:47:01 +0530

    promises

---
+++ main.js
@@ -0,0 +1,52 @@
+// function myFirst(){
+//     myDisplay('good morning')
+// }
+// function mysecond(){
+//     myDisplay('good evening')
+// }
+// myFirst()
+// mySecond()
+
+
+// q1
+function task1(){
+    console.log('hello surya')
+}
+function task2(){
+    console.log('bye surya')
+}
+task1();
+task2();
+
+// q2
+function one(){
+    console.log('1')
+}
+setTimeout(one,1000)
+
+function two(){
+    console.log('2')
+}
+setTimeout(two,1000)
+
+function three(){
+    console.log('3')
+}
+setTimeout(three,1000)
+
+function four(){
+    console.log('3')
+}
+setTimeout(four,1000)
+function five(){
+    console.log('3')
+}
+setTimeout(five,1000)
+
+// Q3
+
+ 
+
+
+
+
