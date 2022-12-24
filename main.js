5cccd6b10af12cbfd872aa132f069b8a9a98ed7a
suryapulicharla8 <suryapulicharla8@gmail.com>
2022-12-24 14:08:52 +0530

    second commit

--- main.js
+++ main.js
@@ -45,6 +45,54 @@ setTimeout(five,1000)
 
 // Q3
 
+function myfun1(){
+    console.log(1)
+}
+setTimeout(myfun1,1000);
+function myfun2(){
+    console.log(2)
+}setTimeout(myfun2,2000);
+
+function myfun3(){
+    console.log(3)
+}setTimeout(myfun3,3000);
+
+function myfun4(){
+    console.log(4)
+}setTimeout(myfun4,4000)
+
+function myfun5(){
+    console.log(5)
+}setTimeout(myfun5,5000)
+
+function myfun6(){
+    console.log(6)
+}setTimeout(myfun6,6000)
+
+function myfun7(){
+    console.log(7)
+}setTimeout(myfun7,7000)
+
+
+
+
+
+
+
+// Q 7
+let myPromise=new Promise((resolve,reject){
+    let x=10;
+    if(x==10){
+        resolve("it is equal to 10")
+    }else{
+        reject("not equal to 10")
+    }
+    
+});
+myPromise.then(resolve);
+myPromise.catch(reject)
+
+
  
 
 
